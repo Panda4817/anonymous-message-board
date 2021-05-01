7dc4e324234bc9c454b199867593dd9d67f58dbb
kanta <kanta.munton@gmail.com>
2021-05-01 18:12:39 +0100

    Add all backend routes and tests

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,196 @@
-'use strict';
+"use strict";
+const ObjectId = require("mongodb").ObjectID;
+const bcrypt = require("bcrypt");
+const saltRounds = 12;
 
-module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+module.exports = function (app, db) {
+	//Sample front-end
+	app.route("/b/:board/").get(function (req, res) {
+		res.sendFile(process.cwd() + "/views/board.html");
+	});
+	app.route("/b/:board/:threadid").get(function (req, res) {
+		res.sendFile(process.cwd() + "/views/thread.html");
+	});
 
+	//Index page (static HTML)
+	app.route("/").get(function (req, res) {
+		res.sendFile(process.cwd() + "/views/index.html");
+	});
+
+	app
+		.route("/api/threads/:board")
+		.post(async (req, res) => {
+			const board = req.body.board;
+			const text = req.body.text;
+			const delete_password = req.body.delete_password;
+			const hash = bcrypt.hashSync(
+				delete_password,
+				saltRounds
+			);
+			const result = await db.insertOne({
+				text: text,
+				delete_password: hash,
+				board: board,
+				replies: [],
+				created_on: new Date(),
+				bumped_on: new Date(),
+				reported: false,
+				reply_count: 0,
+			});
+			res.redirect(`../../../b/${board}/`);
+		})
+		.get((req, res) => {
+			const board = req.params.board;
+			db.find(
+				{ board: board },
+				{
+					sort: { bumped_on: -1 },
+					projection: {
+						replies: { $slice: 3 },
+						reported: 0,
+						delete_password: 0,
+					},
+				}
+			)
+				.limit(10)
+				.toArray((err, result) => {
+					res.json(result);
+				});
+		})
+		.put((req, res) => {
+			const thread_id = req.body.thread_id;
+			db.findOneAndUpdate(
+				{ _id: new ObjectId(thread_id) },
+				{ $set: { reported: true } },
+				{ returnOriginal: false },
+				(err, result) => {
+					res.send("success");
+				}
+			);
+		})
+		.delete((req, res) => {
+			const thread_id = req.body.thread_id;
+			const delete_password = req.body.delete_password;
+			db.findOne(
+				{ _id: new ObjectId(thread_id) },
+				(err, result) => {
+					const isRight = bcrypt.compareSync(
+						delete_password,
+						result.delete_password
+					);
+					if (isRight) {
+						db.deleteOne(
+							{ _id: new ObjectId(thread_id) },
+							(err, result) => {
+								res.send("success");
+							}
+						);
+					} else {
+						res.send("incorrect password");
+					}
+				}
+			);
+		});
+
+	app
+		.route("/api/replies/:board")
+		.post(async (req, res) => {
+			const board = req.params.board;
+			const text = req.body.text;
+			const delete_password = req.body.delete_password;
+			const hash = bcrypt.hashSync(
+				delete_password,
+				saltRounds
+			);
+			const thread_id = req.body.thread_id;
+			const result = await db.findOneAndUpdate(
+				{ _id: new ObjectId(thread_id) },
+				{
+					$push: {
+						replies: {
+							$each: [
+								{
+									_id: new ObjectId(),
+									text: text,
+									created_on: new Date(),
+									delete_password: hash,
+									reported: false,
+								},
+							],
+							$sort: { created_on: -1 },
+						},
+					},
+					$set: { bumped_on: new Date() },
+					$inc: { reply_count: 1 },
+				},
+				{ returnOriginal: false }
+			);
+
+			res.redirect(`../../../b/${board}/${thread_id}`);
+		})
+		.get((req, res) => {
+			const thread_id = req.query.thread_id;
+			db.findOne(
+				{ _id: new ObjectId(thread_id) },
+				{
+					projection: { reported: 0, delete_password: 0 },
+				},
+				(err, result) => {
+					res.json(result);
+				}
+			);
+		})
+		.put((req, res) => {
+			const thread_id = req.body.thread_id;
+			const reply_id = req.body.reply_id;
+			db.findOneAndUpdate(
+				{
+					_id: new ObjectId(thread_id),
+					"replies._id": new ObjectId(reply_id),
+				},
+				{ $set: { "replies.$.reported": true } },
+				{ returnOriginal: false },
+				(err, result) => {
+					res.send("success");
+				}
+			);
+		})
+		.delete((req, res) => {
+			const thread_id = req.body.thread_id;
+			const reply_id = req.body.reply_id;
+			const delete_password = req.body.delete_password;
+			db.findOne(
+				{
+					_id: new ObjectId(thread_id),
+					"replies._id": new ObjectId(reply_id),
+				},
+				{ projection: { "replies.$": 1 } },
+				(err, result) => {
+					const isRight = bcrypt.compareSync(
+						delete_password,
+						result.replies[0].delete_password
+					);
+					if (isRight) {
+						db.findOneAndUpdate(
+							{
+								_id: new ObjectId(thread_id),
+								"replies._id": new ObjectId(reply_id),
+							},
+							{
+								$set: {
+									"replies.$.text":  "[deleted]",
+								},
+								$inc: { reply_count: -1 },
+							},
+							{ returnOriginal: false },
+							(err, result) => {
+								res.send("success");
+							}
+						);
+					} else {
+						res.send("incorrect password");
+					}
+				}
+			);
+		});
 };
--- server.js
+++ server.js
@@ -1,65 +1,64 @@
-'use strict';
-require('dotenv').config();
-const express     = require('express');
-const cors        = require('cors');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require("./routes/api.js");
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner");
 
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(helmet.frameguard({ action: "deny" }))
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.referrerPolicy({ policy: "same-origin" }));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(
+	"/public",
+	express.static(process.cwd() + "/public")
+);
+
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Sample front-end
-app.route('/b/:board/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/board.html');
-  });
-app.route('/b/:board/:threadid')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/thread.html');
-  });
-
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
-
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);
+myDB(async (client) => {
+	// Database
+	const db = await client
+		.db("message_board")
+		.collection("threads");
+	//Routing for API
+	apiRoutes(app, db);
 
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+	//404 Not Found Middleware
+	app.use(function (req, res, next) {
+		res.status(404).type("text").send("Not Found");
+	});
+
+	//Start our server and tests!
+	app.listen(process.env.PORT || 3000, function () {
+		console.log("Listening on port " + process.env.PORT);
+		if (process.env.NODE_ENV === "test") {
+			console.log("Running Tests...");
+			setTimeout(function () {
+				try {
+					runner.run();
+				} catch (e) {
+					var error = e;
+					console.log("Tests are not valid:");
+					console.log(error);
+				}
+			}, 1500);
+		}
+	});
+
+	module.exports = {app: app, db: db};
 });
 
-//Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 1500);
-  }
-});
-
-module.exports = app; //for testing
+// module.exports = {app: app, db: db}; //for testing
